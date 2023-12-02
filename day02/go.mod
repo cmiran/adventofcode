c4f68cb588faa4e6e43949a092f7ec106f65bc99
César Miran <csarmiran@gmail.com>
2023-12-02 08:29:07 +0100

    day02 part1

---
+++ day02/go.mod
@@ -0,0 +1,3 @@
+module adventofcode/day02
+
+go 1.21.4
---
+++ day02/main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+const (
+	red   = 12
+	green = 13
+	blue  = 14
+)
+
+type Set struct {
+	Red   int
+	Blue  int
+	Green int
+}
+
+func splitLine(line, r string) []string {
+	re := regexp.MustCompile(r)
+	return re.Split(line, -1)
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("Please provide a file path")
+	}
+
+	filePath := os.Args[1]
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	fileScanner := bufio.NewScanner(file)
+	fileScanner.Split(bufio.ScanLines)
+
+	result := 0
+
+	for fileScanner.Scan() {
+		line := fileScanner.Text()[5:]
+
+		content := splitLine(line, ": ")
+		id, _ := strconv.Atoi(content[0])
+		sets := splitLine(content[1], "; ")
+		s1 := Set{}
+		ok := true
+
+		for _, set := range sets {
+			cubes := splitLine(set, ", ")
+			s2 := Set{}
+
+			for _, cube := range cubes {
+				content := splitLine(cube, " ")
+				x, _ := strconv.Atoi(content[0])
+
+				switch content[1] {
+				case "red":
+					if s2.Red += x; s2.Red > s1.Red {
+						s1.Red = s2.Red
+					}
+				case "blue":
+					if s2.Blue += x; s2.Blue > s1.Blue {
+						s1.Blue = s2.Blue
+					}
+				case "green":
+					if s2.Green += x; s2.Green > s1.Green {
+						s1.Green = s2.Green
+					}
+				}
+			}
+
+			if s1.Red > red || s1.Green > green || s1.Blue > blue {
+				ok = false
+			}
+		}
+
+		if ok {
+			result += id
+		}
+	}
+
+	fmt.Println(result)
+}
