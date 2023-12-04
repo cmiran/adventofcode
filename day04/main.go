5aa201102f91d3e0fbfa5cbb4516e7fc7a5170b2
César Miran <csarmiran@gmail.com>
2023-12-04 12:56:22 +0100

    day04 part2

--- day04/main.go
+++ day04/main.go
@@ -6,8 +6,30 @@ import (
 	"log"
 	"os"
 	"regexp"
+	"strconv"
 	"strings"
 )
+
+type Scratchcard struct {
+	winningNumbers []string
+	myNumbers      []string
+}
+
+func numberOfGames(scratchcards map[int]Scratchcard, i int, result *int) {
+	mb := make(map[string]struct{}, len(scratchcards[i].myNumbers))
+	for _, x := range scratchcards[i].myNumbers {
+		mb[x] = struct{}{}
+	}
+
+	j := 0
+	for _, x := range scratchcards[i].winningNumbers {
+		if _, found := mb[x]; found {
+			*result++
+			j++
+			numberOfGames(scratchcards, i+j, result)
+		}
+	}
+}
 
 func main() {
 	if len(os.Args) < 2 {
@@ -28,32 +50,24 @@ func main() {
 
 	re1 := regexp.MustCompile(`Card\s+\d+: `)
 	re2 := regexp.MustCompile(`\s+`)
+	re3 := regexp.MustCompile(`\d+`)
 	result := 0
+	scratchcards := make(map[int]Scratchcard)
 
 	for fileScanner.Scan() {
 		line := fileScanner.Text()
+		x := re3.FindString(line)
+		i, _ := strconv.Atoi(x)
 		line = re1.ReplaceAllString(line, "${1}")
 		scratchcard := strings.Split(line, " | ")
 		winningNumbers := re2.Split(scratchcard[0], -1)
 		myNumbers := re2.Split(scratchcard[1], -1)
+		scratchcards[i] = Scratchcard{winningNumbers, myNumbers}
+		result++
+	}
 
-		mb := make(map[string]struct{}, len(myNumbers))
-		for _, x := range myNumbers {
-			mb[x] = struct{}{}
-		}
-
-		points := 0
-		for _, x := range winningNumbers {
-			if _, found := mb[x]; found {
-				if points == 0 {
-					points++
-				} else {
-					points *= 2
-				}
-			}
-		}
-
-		result += points
+	for i := 1; i <= len(scratchcards); i++ {
+		numberOfGames(scratchcards, i, &result)
 	}
 
 	fmt.Println(result)
